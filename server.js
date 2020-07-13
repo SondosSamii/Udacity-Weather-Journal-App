a9afef448c9afa8aef2ecff1de50ddd006ef8b3f
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-07-13 23:32:48 +0200

    Add GET and POST Routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require("express");
 // Start up an instance of app
 const app = express();
 
-/* Middleware*/
+/* Middleware */
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,4 +26,29 @@ const server = app.listen(port, runningServer);
 
 function runningServer() {
   console.log(`Server is running on localhost: ${port}`);
+}
+
+/* GET ROUTE */
+app.get("/all", getData);
+
+function getData(req, res) {
+  req.send(projectData);
+  console.log(projectData);
+}
+
+/* POST ROUTE */
+app.post("/sendWeatherData", sendWeatherData);
+
+function sendWeatherData(req, res) {
+  body = req.body;
+  console.log(body);
+
+  newWeather = {
+    temperature: body.temperature,
+    date: body.date,
+    userResponse: body.userResponse,
+  };
+  projectData.push(newWeather);
+  res.send(newWeather);
+  console.log(newWeather);
 }
